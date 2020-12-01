cd3a5ea43d6f46e60d8427a771b3361bf33548a0
Justin Malandruccolo <justin.malandruccolo@braze.com>
2020-11-30 22:01:31 -0600

    Setting up custom subclass view for In-App Message Modals
    * setting up custom picker view for modals
    
    * saving custom attribute from IAM
    
    * set up picker view to handle string type, removed client side request
    
    * mark
    
    * re-organized
    
    * converted strings to enums

--- Braze Demo/AppboyManager.swift
+++ Braze Demo/AppboyManager.swift
@@ -25,6 +25,8 @@ class AppboyManager: NSObject {
       Appboy.sharedInstance()?.pushAuthorization(fromUserNotificationCenter: granted)
     }
     UIApplication.shared.registerForRemoteNotifications()
+    
+    Appboy.sharedInstance()?.inAppMessageController.inAppMessageUIController?.setInAppMessageUIDelegate?(self)
   }
   
   /// Initialized as the value for the ABKIDFADelegateKey.
@@ -102,6 +104,26 @@ extension AppboyManager {
   
   func logPurchase(productIdentifier: String, inCurrency currency: String, atPrice price: String, withQuanitity quanity: Int) {
     Appboy.sharedInstance()?.logPurchase(productIdentifier, inCurrency: currency, atPrice: NSDecimalNumber(string: price), withQuantity: UInt(quanity))
+  }
+}
+
+// MARK: - ABKInAppMessage UI Delegate
+extension AppboyManager: ABKInAppMessageUIDelegate {
+  func inAppMessageViewControllerWith(_ inAppMessage: ABKInAppMessage) -> ABKInAppMessageViewController {
+    switch inAppMessage {
+    case is ABKInAppMessageFull:
+      return ABKInAppMessageFullViewController(inAppMessage: inAppMessage)
+    case is ABKInAppMessageSlideup:
+      return ABKInAppMessageViewController(inAppMessage: inAppMessage)
+    case is ABKInAppMessageModal:
+      return modalViewController(inAppMessage: inAppMessage)
+    case is ABKInAppMessageHTML:
+      return ABKInAppMessageHTMLViewController(inAppMessage: inAppMessage)
+    case is ABKInAppMessageImmersive:
+      return ABKInAppMessageImmersiveViewController(inAppMessage: inAppMessage)
+    default:
+      return ABKInAppMessageViewController(inAppMessage: inAppMessage)
+    }
   }
 }
 
@@ -236,4 +258,38 @@ extension Notification.Name {
   static let defaultAppExperience = Notification.Name("kDefaultApExperience")
   static let homeScreenContentCard = Notification.Name("kHomeScreenContentCard")
   static let reorderHomeScreen = Notification.Name("kReorderHomeScreen")
+}
+
+// MARK: - Modal In App Message
+class ModalViewController: ABKInAppMessageModalViewController {
+  
+  // MARK: - Outlets
+  @IBOutlet private weak var primaryButton: ABKInAppMessageUIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let immersiveMessage = inAppMessage as? ABKInAppMessageImmersive, let buttons = immersiveMessage.buttons {
+      switch buttons.count {
+      case 1:
+        primaryButton.titleLabel?.text = buttons[0].buttonText
+      case 2:
+        primaryButton.titleLabel?.text = buttons[1].buttonText
+      default:
+        break
+      }
+    }
+  }
+}
+
+// MARK: - In-App Message View Controller Helper
+private extension AppboyManager {
+  func modalViewController(inAppMessage: ABKInAppMessage) -> ABKInAppMessageModalViewController {
+    switch inAppMessage.extras?[InAppMessageKey.viewType.rawValue] as? String {
+    case InAppMessageViewType.picker.rawValue:
+      return ModalPickerViewController(inAppMessage: inAppMessage)
+    default:
+      return ABKInAppMessageModalViewController(inAppMessage: inAppMessage)
+    }
+  }
 }
