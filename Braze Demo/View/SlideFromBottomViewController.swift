60fc58500e200d40c9296562c842eede5aad9c55
malandr2 <justin.malandruccolo@braze.com>
2020-12-01 20:54:05 -0600

    added comments

--- Braze Demo/View/SlideFromBottomViewController.swift
+++ Braze Demo/View/SlideFromBottomViewController.swift
@@ -8,10 +8,12 @@ class SlideFromBottomViewController: SlideupViewController {
 
 // MARK: - View Lifecycle
 extension SlideFromBottomViewController {
+  /// Overriding this method to provide our own custom value for `slideConstraint`
   override func beforeMoveInAppMessageViewOnScreen() {
     setSlideConstraint()
   }
   
+  // When subclassing ABKInAppMessageSlideupViewController, we have to manually handle orienation changes
   override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
     super.viewWillTransition(to: size, with: coordinator)
     coordinator.animate(alongsideTransition: { context in
