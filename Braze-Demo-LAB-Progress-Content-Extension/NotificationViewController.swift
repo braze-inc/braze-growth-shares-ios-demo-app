f6ba75f3adc77c0e3a413d45c37417982db3bba6
malandr2 <justin.malandruccolo@braze.com>
2021-02-24 17:11:44 -0600

    set up dynamic count

--- Braze-Demo-LAB-Progress-Content-Extension/NotificationViewController.swift
+++ Braze-Demo-LAB-Progress-Content-Extension/NotificationViewController.swift
@@ -5,45 +5,69 @@ import UserNotificationsUI
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
   // MARK: - Outlets
-  @IBOutlet weak var stackView: UIStackView!
-  @IBOutlet weak var nextSessionLabel: UILabel!
-  @IBOutlet weak var completeNextSessionByLabel: UILabel!
-    
+  @IBOutlet private weak var stackView: UIStackView!
+  @IBOutlet private weak var nextSessionLabel: UILabel!
+  @IBOutlet private weak var completeNextSessionByLabel: UILabel!
+  
   // MARK: - Variables
+  private var completedSessions: Int = 0
+  private var totalSessions: Int = 0 {
+    didSet {
+      rowCount = totalSessions / columnCount
+      
+      if totalSessions % columnCount != 0 {
+        rowCount += 1
+      }
+    }
+  }
+  
+  private var rowCount: Int = 0
   private var columnCount: Int {
     return traitCollection.verticalSizeClass == .compact ? 6 : 4
   }
   
   func didReceive(_ notification: UNNotification) {
-    configureSessionProgress(with: notification.request.content.userInfo)
-    configureNextSessionLabels(with: notification.request.content.userInfo)
+    let userInfo = notification.request.content.userInfo
+    
+    guard let completedSessionsString = userInfo["session_completed_count"] as? String,
+          let totalSessionString = userInfo["session_total_count"] as? String,
+          let completedSessions = Int(completedSessionsString),
+          let totalSessions = Int(totalSessionString)
+    else { fatalError("Key-Value Pairs are incorrect.")}
+    
+    self.completedSessions = completedSessions
+    self.totalSessions = totalSessions
+    
+    configureNextSessionLabels(with: userInfo)
+    configureTotalSessionProgress()
   }
 }
 
 // MARK: - Private methods
 private extension NotificationViewController {
-  func configureSessionProgress(with userInfo: [AnyHashable: Any]) {
-    guard let sessionsCompletedString = userInfo["session_completed_count"] as? String,
-          let totalSessionString = userInfo["session_total_count"] as? String,
-          let sessionsCompleted = Int(sessionsCompletedString),
-          let totalSessions = Int(totalSessionString)
-    else { fatalError("Key-Value Pairs are incorrect.")}
+  func configureTotalSessionProgress() {
+    var sessionCount = 1
     
-    var sessions = 1
-    for _ in 0..<2 {
+    for _ in 0..<rowCount {
       let sessionStackView = UIStackView()
       sessionStackView.axis = .horizontal
       sessionStackView.distribution = .fillEqually
       sessionStackView.spacing = 10
       
-      for _ in 0..<4 {
+      for _ in 0..<columnCount {
         let sessionView: SessionView = .fromNib()
-        sessionView.configureView(String(sessions), isCompleted: sessions <= sessionsCompleted)
-        sessionStackView.addArrangedSubview(sessionView)
         
+        if sessionCount <= totalSessions {
+          sessionView.configureView(String(sessionCount), isCompleted: sessionCount <= completedSessions)
+        } else {
+          sessionView.configureBlankView()
+        }
+        
+        sessionStackView.addArrangedSubview(sessionView)
         sessionView.translatesAutoresizingMaskIntoConstraints = false
         sessionView.widthAnchor.constraint(equalTo: sessionView.heightAnchor, multiplier: 1).isActive = true
-        sessions += 1
+        
+        sessionCount += 1
       }
       
       stackView.addArrangedSubview(sessionStackView)
--- Braze-Demo-LAB-Progress-Content-Extension/SessionView.swift
+++ Braze-Demo-LAB-Progress-Content-Extension/SessionView.swift
@@ -2,21 +2,27 @@ import UIKit
 
 class SessionView: UIView {
   
+  // MARK: - Outlets
   @IBOutlet private var sessionNumberLabel: UILabel!
-  private var isACircle = false
   
-  func configureView(_ sessionString: String?, isCompleted: Bool) {
-    sessionNumberLabel.text = sessionString
-    sessionNumberLabel.textColor = isCompleted ? .white : .black
-    
-    backgroundColor = isCompleted ? .systemBlue : .systemGray
-    
-  }
+  // MARK: - Variables
+  private var isACircle = false
   
   override func layoutSubviews() {
     guard !isACircle else { return }
     
     layer.cornerRadius = frame.size.width / 2
     isACircle = true
+  }
+  
+  func configureView(_ sessionString: String?, isCompleted: Bool) {
+    sessionNumberLabel.text = sessionString
+    sessionNumberLabel.textColor = isCompleted ? .white : .black
+    
+    backgroundColor = isCompleted ? .systemGreen : .systemGray
+  }
+  
+  func configureBlankView() {
+    backgroundColor = .clear
   }
 }
