f0a648a57c694de865fe466b1216f9923ae5c616
malandr2 <justin.malandruccolo@braze.com>
2021-02-28 11:10:06 -0600

    cleaned up

--- Braze-Demo-LAB-Register-Content-Extension/NotificationViewController.swift
+++ Braze-Demo-LAB-Register-Content-Extension/NotificationViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import UserNotifications
 import UserNotificationsUI
 
-class NotificationViewController: UIViewController, UNNotificationContentExtension {
+class NotificationViewController: UIViewController {
 
   // MARK: - Outlets
   @IBOutlet private weak var imageView: UIImageView!
@@ -14,16 +14,24 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
   
   // MARK: - Variables
   private let registerIdentifier = "REGISTER"
+  private lazy var registerAction: UNNotificationAction = {
+    UNNotificationAction(identifier: registerIdentifier, title: registerIdentifier.capitalized, options: .authenticationRequired)
+  }()
+  
   override var canBecomeFirstResponder: Bool {
     return true
   }
-    
+}
+
+// MARK: - Notification Content Extension Methods
+extension NotificationViewController: UNNotificationContentExtension {
   func didReceive(_ notification: UNNotification) {
     let content = notification.request.content
-      
+    
     if let attachment = content.attachments.first {
       if attachment.url.startAccessingSecurityScopedResource() {
-        if let data = try? Data(contentsOf: attachment.url), let image = UIImage(data: data) {
+        if let data = try? Data(contentsOf: attachment.url),
+           let image = UIImage(data: data) {
           imageView.image = image
         }
         attachment.url.stopAccessingSecurityScopedResource()
@@ -34,8 +42,8 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
   func didReceive(_ response: UNNotificationResponse, completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
     if response.actionIdentifier == registerIdentifier {
         UINotificationFeedbackGenerator().notificationOccurred(.success)
-        extensionContext?.notificationActions = []
-       
+        configureNotificationActions([])
+     
         displayAllSetView { completion(.dismiss) }
     } else {
       completion(.doNotDismiss)
@@ -56,15 +64,14 @@ extension NotificationViewController: UITextFieldDelegate {
 
 // MARK: - Private Methods
 private extension NotificationViewController {
-  func validateEmail(_ email: String?) {
-    if let email = email, email.isValidEmail {
-      displayRegisterActionButton()
-    }
+  func configureNotificationActions(_ actions: [UNNotificationAction]) {
+    extensionContext?.notificationActions = actions
   }
   
-  func displayRegisterActionButton() {
-    let registerAction = UNNotificationAction(identifier: registerIdentifier, title: registerIdentifier.capitalized, options: .authenticationRequired)
-    extensionContext?.notificationActions = [registerAction]
+  func validateEmail(_ email: String?) {
+    if let email = email, email.isValidEmail {
+      configureNotificationActions([registerAction])
+    }
   }
   
   func displayAllSetView(completion: @escaping () -> ()) {
