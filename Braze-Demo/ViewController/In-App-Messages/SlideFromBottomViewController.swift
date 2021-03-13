d6a7dcbed0b6028b35b5f5b321555a7a88d3422c
malandr2 <justin.malandruccolo@braze.com>
2021-03-13 00:34:32 -0600

    Updated SDK to 4.0.0, refactored offset calculation

--- Braze-Demo/ViewController/In-App-Messages/SlideFromBottomViewController.swift
+++ Braze-Demo/ViewController/In-App-Messages/SlideFromBottomViewController.swift
@@ -3,30 +3,50 @@ import UIKit
 class SlideFromBottomViewController: SlideupViewController {
 
   // MARK: - Variables
+  private let DefaultVerticalMarginHeight: CGFloat = 10
+  
+  private var safeAreaOffset: CGFloat {
+    guard view.superview?.layoutMargins.bottom == 0,
+          bottomSpacing != 0 else { return 0 }
+    
+    return -DefaultVerticalMarginHeight
+  }
+  
   private var bottomSpacing: CGFloat {
-    return BrazeManager.shared.activeApplicationViewController.topMostViewController().view.safeAreaInsets.bottom
+    return BrazeManager.shared.activeApplicationViewController.topMostViewController().view.safeAreaInsets.bottom - BrazeManager.shared.activeApplicationViewController.view.safeAreaInsets.bottom
+  }
+  
+  // MARK: ABK Variables
+  override var offset: CGFloat {
+    get {
+      return bottomSpacing
+    }
+    set {
+      super.offset = newValue
+    }
   }
 }
 
 // MARK: - View Lifecycle
 extension SlideFromBottomViewController {
-  /// Overriding this method to provide our own custom value for `slideConstraint`
   override func beforeMoveInAppMessageViewOnScreen() {
-    setSlideConstraint()
+    super.beforeMoveInAppMessageViewOnScreen()
+    
+    setOffset()
   }
   
   // When subclassing ABKInAppMessageSlideupViewController, we have to manually handle orienation changes
   override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
     super.viewWillTransition(to: size, with: coordinator)
     coordinator.animate(alongsideTransition: { context in
-      self.setSlideConstraint()
+      self.setOffset()
     }, completion: nil)
   }
 }
 
 // MARK: - Private
 private extension SlideFromBottomViewController {
-  func setSlideConstraint() {
-    slideConstraint?.constant = bottomSpacing
+  func setOffset() {
+    offset = (-bottomSpacing - safeAreaOffset)
   }
 }
