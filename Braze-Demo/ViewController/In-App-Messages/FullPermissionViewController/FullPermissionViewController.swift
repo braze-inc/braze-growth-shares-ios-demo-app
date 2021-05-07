cc4ca94cea6a15a488ff304f43765645e75fe6bb
malandr2 <justin.malandruccolo@braze.com>
2021-05-07 11:32:28 -0400

    set up view for permission

--- Braze-Demo/ViewController/In-App-Messages/FullPermissionViewController/FullPermissionViewController.swift
+++ Braze-Demo/ViewController/In-App-Messages/FullPermissionViewController/FullPermissionViewController.swift
@@ -2,6 +2,12 @@ import UIKit
 
 class FullPermissionViewController: FullViewController {
   
+  // MARK: - Outlets
+  @IBOutlet private weak var permissionView: UIView!
+  @IBOutlet private weak var arrowView: UIView!
+  @IBOutlet private weak var permissionTitle: UILabel!
+  @IBOutlet private weak var permissionSubtitle: UILabel!
+  
   override var nibName: String {
     return "FullPermissionViewController"
   }
@@ -11,6 +17,18 @@ class FullPermissionViewController: FullViewController {
 extension FullPermissionViewController {
   override func loadView() {
     Bundle.main.loadNibNamed(nibName, owner: self, options: nil)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let titleText = inAppMessage.extras?["permission_title"] as? String {
+      permissionTitle.text = titleText
+    }
+    
+    if let subtitleText = inAppMessage.extras?["permission_subtitle"] as? String {
+      permissionSubtitle.text = subtitleText
+    }
   }
   
   override func viewWillAppear(_ animated: Bool) {
